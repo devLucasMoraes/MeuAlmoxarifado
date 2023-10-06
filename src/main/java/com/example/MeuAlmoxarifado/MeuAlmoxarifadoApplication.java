985ba8857993dbf5cd46207698c440c62dafbfa5
devLucasMoraes <devlucasmoraes@gmail.com>
2023-10-05 22:31:20 -0300

    Initial commit

---
+++ src/main/java/com/example/MeuAlmoxarifado/MeuAlmoxarifadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeuAlmoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAlmoxarifadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuAlmoxarifadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MeuAlmoxarifado/MeuAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeuAlmoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
