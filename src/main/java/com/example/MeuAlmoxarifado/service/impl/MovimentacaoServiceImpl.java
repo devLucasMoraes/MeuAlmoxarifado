1e28e0cfbbf697c1dac41386cfe02f15a1f9bf37
devLucasMoraes <devlucasmoraes@gmail.com>
2023-10-13 17:45:49 -0300

    feat: implementando movimentacoes

---
+++ src/main/java/com/example/MeuAlmoxarifado/service/impl/MovimentacaoServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.MeuAlmoxarifado.service.impl;
+
+import com.example.MeuAlmoxarifado.service.MovimentacaoService;
+
+public class MovimentacaoServiceImpl implements MovimentacaoService {
+}
